77c420a843a522f22d6790f963c64aaa8b393d45
Neyeska Medina <neyeska.go@gmail.com>
2023-08-15 13:26:52 -0400

    Agregando formato miles a total

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,7 @@
 //Declaro mis funciones
 function calcula_total(){
     let total = Number(quantity.value) * fPrice;
+    total = total.toLocaleString('es-CL'); 
     eTotal.innerHTML = total;
     eQuantity.innerHTML = quantity.value;
     eColor.style.backgroundColor = color.value;
